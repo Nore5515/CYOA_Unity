cb6d6ad7dfd6c560fc3ecf01e92c89cb3b28423f
Noah <Nore5515@gmail.com>
2022-05-11 21:57:34 -0500

    setting up da basics

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene : MonoBehaviour
+{
+    [SerializeField] string sceneTitle;
+    [SerializeField] string sceneContent;
+    
+
+    [SerializeField] Text title;
+    [SerializeField] Text content;
+    [SerializeField] List<Button> choices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
